2d6ff2266cccdfadcffad16035af3ff9d7ab2f3f
BrentBoyMeBob <brent.monning.jr@gmail.com>
2022-02-12 18:12:12 -0800

    dot-suckless: rounded template, status update

--- local/.dwm/status.sh
+++ local/.dwm/status.sh
@@ -1,4 +1,4 @@
 while true; do
-	xsetroot -name "墳 $(pactl list sinks | awk '$1=="Volume:" {print $5}')   $(($(brightnessctl g) / 255 * 100))%  拉 $(cat /sys/class/power_supply/BAT0/capacity)%   $(date +%I:%M)   $(date +%D) "
+	xsetroot -name "墳 $(pactl list sinks | awk '$1=="Volume:" {print $5}')  拉 $(cat /sys/class/power_supply/BAT0/capacity)%   $(date +%I:%M)   $(date +%D) "
 	sleep 2
 done
