5b362230dc14b1d473d80d2d21d1ea7818eddb21
schrobingus <brent.monning.jr@gmail.com>
2024-09-05 13:48:56 -0700

    pull some unused dots

--- nix/home/home.nix
+++ nix/home/home.nix
@@ -27,12 +27,10 @@
 
   # NOTE: Mind the potential `config.lib.file.mkOutOfStoreSymlink` prefix.
   home.file = {
-    # ".config/iterm2/com.googlecode.iterm2.plist".source = config.lib.file.mkOutOfStoreSymlink ../../iterm2.plist;
     ".vimrc".source = ../../rc.vim; # TODO: overhaul vim config as nvim
     ".config/wezterm/wezterm.lua".source = config.lib.file.mkOutOfStoreSymlink ../../wezterm.lua;
     # FIXME: for some reason, the mkOutOfStoreSymlink flag aint doing anything here
     ".zshrc".source = config.lib.file.mkOutOfStoreSymlink ../../rc.zsh;
-    # "Library/Application Support/Code/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink ../../vscode-settings.json; 
   };
 
   home.sessionVariables = {
