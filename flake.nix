88b584f7da95c5b61995c7e0d673f062997a4ebb
schrobingus <brent.monning.jr@gmail.com>
2024-09-24 10:47:40 -0700

    reference nixvim flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,134 +11,129 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixvim = {
-      url = "github:nix-community/nixvim/nixos-24.05";
-      inputs.nixpkgs.follows = "nixpkgs-stable";
-    };
+    nixvim-config.url = "github:schrobingus/nixvim-config";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-stable, nix-darwin, home-manager, nixvim } @ inputs: 
-  let
+  outputs = { 
+    self, 
+    nixpkgs, 
+    nix-darwin, 
+    home-manager, 
+    nixvim-config
+  } @ inputs: let
     mkNixOSConfig = { 
       system, 
       extraHomeModules ? [], 
       extraNixOSModules ? [] 
-    }:
-    let
+    }: let
       pkgs = import nixpkgs {
         inherit system;
       };
-    in
-    nixpkgs.lib.nixosSystem {
-      system = system;
-      specialArgs = { inherit self; };
-      modules = 
-      [
-        ./nix/nixos/system.nix
-      ]
-      ++ extraNixOSModules
-      ++ [
-        home-manager.nixosModules.home-manager {
-          home-manager.extraSpecialArgs = { inherit inputs; };
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.brent = {
-            imports = [
-              ./nix/home/home.nix
-            ] ++ extraHomeModules;
-          };
-        }
-      ];
-    };
+    in nixpkgs.lib.nixosSystem {
+        system = system;
+        specialArgs = { inherit self; };
+        modules = 
+        [
+          ./nix/nixos/system.nix
+        ]
+        ++ extraNixOSModules
+        ++ [
+          home-manager.nixosModules.home-manager {
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.brent = {
+              imports = [
+                ./nix/home/home.nix
+                {
+                  home.packages = [
+                    pkgs.home-manager
+                    inputs.nixvim-config.packages.${system}.default
+                  ];
+                }
+              ] ++ extraHomeModules;
+            };
+          }
+        ];
+      };
+
     mkDarwinConfig = { 
       system, 
       extraHomeModules ? [], 
       extraDarwinModules ? [] 
-    }:
-    let
+    }: let
       pkgs = import nixpkgs {
         inherit system;
       };
-    in 
-    nix-darwin.lib.darwinSystem {
-      system = system;
-      specialArgs = { inherit self; };
-      modules = 
-      [ ./nix/darwin/configuration.nix ]
-      ++ extraDarwinModules
-      ++ [
-        home-manager.darwinModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = { inherit inputs; };
-          home-manager.users.brent = {
-            imports = [
-              ./nix/home/home.nix
-            ] ++ extraHomeModules;
-          };
-        }
-      ];
-    };
+    in nix-darwin.lib.darwinSystem {
+        system = system;
+        specialArgs = { inherit self; };
+        modules = 
+        [ ./nix/darwin/configuration.nix ]
+        ++ extraDarwinModules
+        ++ [
+          home-manager.darwinModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.users.brent = {
+              imports = [
+                ./nix/home/home.nix
+                {
+                  home.packages = [
+                    pkgs.home-manager
+                    inputs.nixvim-config.packages.${system}.default
+                  ];
+                }
+              ] ++ extraHomeModules;
+            };
+          }
+        ];
+      };
+
     mkHomeConfig = { 
       system, 
       extraHomeModules ? [] 
-    }: 
-    let
+    }: let
       pkgs = import nixpkgs {
         inherit system;
       };
-    in 
-    home-manager.lib.homeManagerConfiguration {
-      pkgs = pkgs;
-      extraSpecialArgs = { inherit self; };
-      modules = [
-        ./nix/home/home.nix
-        {
-          nix.package = pkgs.nix;
-          home.packages = [
-            pkgs.home-manager
+    in home-manager.lib.homeManagerConfiguration {
+        pkgs = pkgs;
+        extraSpecialArgs = { inherit self; };
+        modules = [
+          ./nix/home/home.nix
+          {
+            nix.package = pkgs.nix;
+            home.packages = [
+              pkgs.home-manager
+              inputs.nixvim-config.packages.${system}.default
+            ];
+          }
+        ] ++ extraHomeModules;
+      };
+  in {
+      # Each configuration is segregated by hostname.
+      darwinConfigurations = {
+        "chaos" = mkDarwinConfig {  # "chaos" is an M1 Macbook Air from 2020.
+          system = "aarch64-darwin";
+          extraDarwinModules = [
+            ./nix/darwin/homebrew.nix
+            ./nix/darwin/settings.nix
           ];
-        }
-      ] ++ extraHomeModules;
-    };
-  in 
-  {
-    # Each configuration is segregated by hostname.
-    darwinConfigurations = {
-      "chaos" = mkDarwinConfig {  # "chaos" is an M1 Macbook Air from 2020.
-      system = "aarch64-darwin";
-      extraDarwinModules = [
-        ./nix/darwin/homebrew.nix
-        ./nix/darwin/settings.nix
-      ];
-      extraHomeModules = [ 
-        ./nix/home/zsh.nix
-        ./nix/home/nvim.nix
-      ];
-    };
-  };
-  homeConfigurations = {
-    "floppa" = mkHomeConfig { # "floppa" is a Lenovo Chromebook Duet (krane) running postmarketOS.
-    system = "aarch64-linux";
-    extraHomeModules = [
-      ./nix/home/zsh.nix
-    ];
-  };
-};
-nixosConfigurations = {
-  "thonk" = mkNixOSConfig { # "thonk" is a Thinkpad X131e Chromebook running NixOS on Coreboot. It also cost only 45 dollars, what a bargain!
-  system = "x86_64-linux";
-  extraNixOSModules = [
-          # TODO: figure out a way to handle cachix
-          # /etc/nixos/cachix.nix
-          ./nix/nixos/hardware-configuration.nix
-        ];
-        extraHomeModules = [
-          ./nix/home/fonts.nix
-          ./nix/home/zsh.nix
-          ./nix/home/nvim.nix
-        ];
+          extraHomeModules = [ 
+            ./nix/home/zsh.nix
+          ];
+        };
+      };
+
+      homeConfigurations = {
+        "floppa" = mkHomeConfig { # "floppa" is a Lenovo Chromebook Duet (krane) running postmarketOS.
+          system = "aarch64-linux";
+          extraHomeModules = [
+            ./nix/home/zsh.nix
+          ];
+        };
       };
     };
-  };
 }
