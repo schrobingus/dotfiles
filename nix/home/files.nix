0108e7c7f5592997f5c4601999a0baacb36198df
Brent Monning <brent.monning.jr@gmail.com>
2025-08-12 11:42:34 -0700

    reorganized dotfile structure and made linking dynamic

--- nix/home/files.nix
+++ nix/home/files.nix
@@ -1,29 +1,23 @@
-{ config, dotDir, ... }:
 
-{
-  home.file = {
-    ".vimrc".source = config.lib.file.mkOutOfStoreSymlink "${dotDir}/config/vimrc";
-    # ".zshrc".source = config.lib.file.mkOutOfStoreSymlink "${dotDir}/config/zshrc";
-  };
+{ config, lib, dotDir, ... }:
 
-  xdg.configFile = {
-    # "alacritty.toml".source = config.lib.file.mkOutOfStoreSymlink "${dotDir}/config/alacritty.toml";
-    "wezterm/wezterm.lua".source = config.lib.file.mkOutOfStoreSymlink "${dotDir}/config/wezterm.lua";
-    "ghostty".source = config.lib.file.mkOutOfStoreSymlink "${dotDir}/config/ghostty";
+let
 
-    "wallpaper.jpg".source = config.lib.file.mkOutOfStoreSymlink "${dotDir}/config/wallpaper.jpg";
-    "dunst/dunstrc".source = config.lib.file.mkOutOfStoreSymlink "${dotDir}/config/dunstrc";
-    "i3/config".source = config.lib.file.mkOutOfStoreSymlink "${dotDir}/config/i3-config";
-    "i3status/config".source = config.lib.file.mkOutOfStoreSymlink "${dotDir}/config/i3status-config";
-    "picom.conf".source = config.lib.file.mkOutOfStoreSymlink "${dotDir}/config/picom.conf";
-    "zsh/rc.zsh".source = config.lib.file.mkOutOfStoreSymlink "${dotDir}/config/zshrc";
+  # FIXME: this function works well, but because it symlinks directories, it often catches junk files in the crossfire. for now, just throw the junk files in gitignore, but consider linking the individual files
 
-    "tt-schemes" = {
-      recursive = true;
-      source = builtins.fetchGit {
-        url = "https://github.com/tinted-theming/schemes";
-        rev = "61058a8d2e2bd4482b53d57a68feb56cdb991f0b";
-      };
-    };
-  };
+  mkLinks = paths:
+    lib.attrsets.mergeAttrsList (map (relPath: {
+      "${relPath}".source =
+        config.lib.file.mkOutOfStoreSymlink "${dotDir}/togohome/${relPath}";
+    }) paths);
+
+  topLevel = builtins.attrNames (builtins.readDir "${dotDir}/togohome");
+  configLevel = builtins.attrNames (builtins.readDir "${dotDir}/togohome/.config");
+
+  homeFiles = builtins.filter (name: name != ".config") topLevel;
+  configFiles = map (name: ".config/${name}") configLevel;
+
+in {
+  home.file = mkLinks (homeFiles ++ configFiles);
 }
+
